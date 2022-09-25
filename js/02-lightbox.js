bca5cfe6a2571f1d56b37a8beb9eb915a1caa2fe
serhiikravchak <105275364+serhiikravchak@users.noreply.github.com>
2022-09-25 12:17:15 +0200

    hw 7 task 2 added

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemMarkup);
+
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captions: true,
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+
+ })
